93fb75ece05356f4e5cd4cba2af510a346e38994
Mike Schmidt <mts@colorado.edu>
2011-05-15 14:26:50 -0600

    Changed all gene and curve attributes to be complex.

--- src/curve.c
+++ src/curve.c
@@ -7,43 +7,72 @@
 #include <gsl/gsl_spline.h>
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_vector.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_complex_math.h>
+
 
 /*  curve.c: Contains all the curve generation rutines.
  */
 
 
 
-/* We generate the curve from a curve-gene. 
- * The gene is a two dimensional array that has the location in gene_x
- * and the height in gene_y. As we add more points we refine our curve.
- */
 
-int gene2curve ( double width, gsl_vector * gene_x, gsl_vector * gene_y, gsl_vector * curve_x, gsl_vector * curve_y)
+int gene2curve ( double dx, unsigned int gene_size, gsl_vector * gene_x, gsl_vector_complex * gene_y, gsl_vector * curve_x, gsl_vector_complex * curve_y)
 {
-    unsigned int size       = gene_x->size;
+    printf("#Entering gene2curve\n");
+    /* We generate the curve from a curve-gene. 
+     * The gene is a two dimensional array that has the location in gene_x
+     * and the height in gene_y. As we add more points we refine our curve.
+     */
     unsigned int curve_size = curve_x->size;
-    double dx         = width/curve_size;
-    gsl_interp_accel *acc = gsl_interp_accel_alloc ();
-    gsl_spline *spline    = gsl_spline_alloc (gsl_interp_cspline, size);
-    gsl_spline_init (spline, gene_x->data, gene_y->data , size);
     int i;
+    gsl_interp_accel *acc      = gsl_interp_accel_alloc ();
+    gsl_spline *spline_real    = gsl_spline_alloc (gsl_interp_cspline, gene_size);
+    gsl_spline *spline_imag    = gsl_spline_alloc (gsl_interp_cspline, gene_size);
+    //Let us do the real parts first
+    printf("#gene2curve: Finding Spline on real part\n");
+    gsl_vector * gene_y_temp = gsl_vector_alloc (gene_size);
+    for(i=0; i < gene_size;i++)
+        gsl_vector_set (gene_y_temp,i,GSL_REAL(gsl_vector_complex_get(gene_y,i)));
+
+    gsl_spline_init (spline_real, gene_x->data, gene_y_temp->data , gene_size);
+
+    //Now for the imag parts
+    printf("#gene2curve: Finding Spline on imag part\n");
+    for(i=0; i < gene_size;i++)
+        gsl_vector_set (gene_y_temp,i,GSL_IMAG(gsl_vector_complex_get(gene_y,i)));
+
+    gsl_spline_init (spline_imag, gene_x->data, gene_y_temp->data , gene_size);
+    
+    printf("#gene2curve: Writing Spline data to curve\n");
     for(i=0; i < curve_x->size; i++)
     {
-        gsl_vector_set (curve_x,i,(double)i*dx);
-        gsl_vector_set (curve_y,i,gsl_spline_eval (spline,(double)i*dx,acc));
+        gsl_vector_complex_set (curve_y,i,
+            gsl_complex_rect (
+                gsl_spline_eval (spline_real,(double)i*dx,acc),
+                gsl_spline_eval (spline_imag,(double)i*dx,acc)
+            )
+        );
     }
-    gsl_spline_free (spline);
+    printf("#gene2curve: Freeing allocated workspace\n");
+    gsl_spline_free (spline_real);
+    gsl_spline_free (spline_imag);
     gsl_interp_accel_free (acc);
 
     return 0;
 }
 
-int print_curve (gsl_vector * curve_x, gsl_vector * curve_y)
+void print_curve (gsl_vector * curve_x, gsl_vector_complex * curve_y)
 {
+    printf("#Entering print_curve\n");
     int i;
     for(i=0;i < curve_x->size ;i++)
     {
-        printf("%g %g\n",gsl_vector_get (curve_x,i), gsl_vector_get (curve_y,i) );
+        printf("%g %g\n",gsl_vector_get (curve_x,i), GSL_REAL(gsl_vector_complex_get (curve_y,i)) );
     }
-    return 0;
+    printf("\n");
+    for(i=0;i < curve_x->size ;i++)
+    {
+        printf("%g %g\n",gsl_vector_get (curve_x,i), GSL_IMAG(gsl_vector_complex_get (curve_y,i)) );
+    }
 }
--- src/main.c
+++ src/main.c
@@ -7,8 +7,9 @@
 #include <gsl/gsl_blas.h>
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_matrix.h>
+#include <gsl/gsl_complex_math.h>
 
-#define MAX_GENE_SIZE 5
+#define MAX_GENE_SIZE 100
 #define CURVE_SIZE 1000
 
 
@@ -17,41 +18,49 @@ int main (void)
     double width = 1.0;
     double dx    = width/CURVE_SIZE;
     int i;
-    gsl_vector * gene_x  = gsl_vector_alloc (MAX_GENE_SIZE);
-    gsl_vector * gene_y  = gsl_vector_alloc (MAX_GENE_SIZE);
-    gsl_vector * curve_x = gsl_vector_alloc (CURVE_SIZE);
-    gsl_vector * curve_y = gsl_vector_alloc (CURVE_SIZE);
-    for(i=0; i < MAX_GENE_SIZE; i++)
-    {
-        gsl_vector_set (gene_x, i,1);
-        gsl_vector_set (gene_y, i,0);
-    }
-    for(i=0; i < CURVE_SIZE; i++)
-    {
-        gsl_vector_set (curve_x, i,0);
-        gsl_vector_set (curve_y, i,0);
-    }
-    // Set Pinning on LHS
-    gsl_vector_set (gene_x, 0,0);
-    gsl_vector_set (gene_y, 0,0);
-    gsl_vector_set (gene_x, 1,dx);
-    gsl_vector_set (gene_y, 1,0);
-    // Set points
-    gsl_vector_set (gene_x, 2,0.5);
-    gsl_vector_set (gene_y, 2,1.0);
-    // Set Pinning on LHS
-    gsl_vector_set (gene_x, 3,width-dx);
-    gsl_vector_set (gene_y, 3,0);
-    gsl_vector_set (gene_x, 4,width);
-    gsl_vector_set (gene_y, 4,0);
+    unsigned int gene_size = 0;
+    gsl_vector         * gene_x  = gsl_vector_alloc (MAX_GENE_SIZE);
+    gsl_vector_complex * gene_y  = gsl_vector_complex_alloc (MAX_GENE_SIZE);
+    gsl_vector         * curve_x = gsl_vector_alloc (CURVE_SIZE);
+    gsl_vector_complex * curve_y = gsl_vector_complex_alloc (CURVE_SIZE);
+    gsl_vector_set_zero (gene_x);
+    gsl_vector_complex_set_zero (gene_y);
+    gsl_vector_set_all (curve_x,1.0);
+    gsl_vector_complex_set_zero (curve_y);
 
-    /*for(i=0;i<6;i++)
-        printf("%g %g\n",gsl_vector_get (gene_x,i), gsl_vector_get(gene_y,i));
-    */
+    //Fill curve_x with descrete steps
+    for(i=0;i<curve_x->size;i++)
+        gsl_vector_set (curve_x,i,(double)i*dx);
+
+    // Set Pinning on LHS
+    int gene_index = -1;
+    gene_index++;
+    gsl_vector_set (gene_x, gene_index,0);
+    gsl_vector_complex_set (gene_y, gene_index,gsl_complex_rect(0.0,0.0));
+    gene_index++;
+    gsl_vector_set (gene_x, gene_index,dx);
+    gsl_vector_complex_set (gene_y, gene_index,gsl_complex_rect(0.0,0.0));
+    // Set points
+    gene_index++;
+    gsl_vector_set (gene_x, gene_index,0.5);
+    gsl_vector_complex_set (gene_y, gene_index,gsl_complex_rect(1.0,0.7));
+    // Set Pinning on LHS
+    gene_index++;
+    gsl_vector_set (gene_x, gene_index,width-dx);
+    gsl_vector_complex_set (gene_y, gene_index,gsl_complex_rect(0.0,0.0));
+    gene_index++;
+    gsl_vector_set (gene_x, gene_index,width);
+    gsl_vector_complex_set (gene_y, gene_index,gsl_complex_rect(0.0,0.0));
+    
+    //printf("gene_index=%i\n",gene_index);
+    //for(i=0;i<gene_index;i++)
+    //    printf("%g %g+%gi\n",gsl_vector_get (gene_x,i), GSL_REAL(gsl_vector_complex_get(gene_y,i)), GSL_IMAG(gsl_vector_complex_get(gene_y,i)));
+    
 
     // Convert to curve
-    gene2curve(1.0,gene_x,gene_y,curve_x,curve_y);
+    gene2curve(dx,gene_index+1,gene_x,gene_y,curve_x,curve_y);
 
     print_curve(curve_x,curve_y);
+
     return 0;
 }

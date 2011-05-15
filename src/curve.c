71f09e0efa832697526b6c3c90e7228a3ce993cb
Mike Schmidt <mts@colorado.edu>
2011-05-15 01:47:35 -0600

    Woops, forgot to add the files in...

--- src/curve.c
+++ src/curve.c
@@ -1,18 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "../config.h"
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_blas.h>
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_spline.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_vector.h>
 
-/*  This file contains all the curve generation rutines.
- */ 
+/*  curve.c: Contains all the curve generation rutines.
+ */
 
 
-int curve_print(unsigned int length, double dx, double *ap)
+
+/* We generate the curve from a curve-gene. 
+ * The gene is a two dimensional array that has the location in gene_x
+ * and the height in gene_y. As we add more points we refine our curve.
+ */
+
+int gene2curve ( double width, gsl_vector * gene_x, gsl_vector * gene_y, gsl_vector * curve_x, gsl_vector * curve_y)
+{
+    unsigned int size       = gene_x->size;
+    unsigned int curve_size = curve_x->size;
+    double dx         = width/curve_size;
+    gsl_interp_accel *acc = gsl_interp_accel_alloc ();
+    gsl_spline *spline    = gsl_spline_alloc (gsl_interp_cspline, size);
+    gsl_spline_init (spline, gene_x->data, gene_y->data , size);
+    int i;
+    for(i=0; i < curve_x->size; i++)
+    {
+        gsl_vector_set (curve_x,i,(double)i*dx);
+        gsl_vector_set (curve_y,i,gsl_spline_eval (spline,(double)i*dx,acc));
+    }
+    gsl_spline_free (spline);
+    gsl_interp_accel_free (acc);
+
+    return 0;
+}
+
+int print_curve (gsl_vector * curve_x, gsl_vector * curve_y)
 {
     int i;
-    for(i=0;i<length;i++)
+    for(i=0;i < curve_x->size ;i++)
     {
-        printf("%g %g\n",(double)i*dx,*(ap+i));
+        printf("%g %g\n",gsl_vector_get (curve_x,i), gsl_vector_get (curve_y,i) );
     }
     return 0;
 }
--- src/main.c
+++ src/main.c
@@ -1,9 +1,57 @@
+/* This contains the main line of execution for the program.
+ */
+
 #include <stdio.h>
 #include "../config.h"
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_blas.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_matrix.h>
 
-main()
+#define MAX_GENE_SIZE 5
+#define CURVE_SIZE 1000
+
+
+int main (void)
 {
-    printf("soliton_search: Version ...\n");
+    double width = 1.0;
+    double dx    = width/CURVE_SIZE;
+    int i;
+    gsl_vector * gene_x  = gsl_vector_alloc (MAX_GENE_SIZE);
+    gsl_vector * gene_y  = gsl_vector_alloc (MAX_GENE_SIZE);
+    gsl_vector * curve_x = gsl_vector_alloc (CURVE_SIZE);
+    gsl_vector * curve_y = gsl_vector_alloc (CURVE_SIZE);
+    for(i=0; i < MAX_GENE_SIZE; i++)
+    {
+        gsl_vector_set (gene_x, i,1);
+        gsl_vector_set (gene_y, i,0);
+    }
+    for(i=0; i < CURVE_SIZE; i++)
+    {
+        gsl_vector_set (curve_x, i,0);
+        gsl_vector_set (curve_y, i,0);
+    }
+    // Set Pinning on LHS
+    gsl_vector_set (gene_x, 0,0);
+    gsl_vector_set (gene_y, 0,0);
+    gsl_vector_set (gene_x, 1,dx);
+    gsl_vector_set (gene_y, 1,0);
+    // Set points
+    gsl_vector_set (gene_x, 2,0.5);
+    gsl_vector_set (gene_y, 2,1.0);
+    // Set Pinning on LHS
+    gsl_vector_set (gene_x, 3,width-dx);
+    gsl_vector_set (gene_y, 3,0);
+    gsl_vector_set (gene_x, 4,width);
+    gsl_vector_set (gene_y, 4,0);
+
+    /*for(i=0;i<6;i++)
+        printf("%g %g\n",gsl_vector_get (gene_x,i), gsl_vector_get(gene_y,i));
+    */
+
+    // Convert to curve
+    gene2curve(1.0,gene_x,gene_y,curve_x,curve_y);
+
+    print_curve(curve_x,curve_y);
+    return 0;
 }
